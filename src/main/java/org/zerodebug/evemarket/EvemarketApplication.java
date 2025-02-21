1eb2be2220efae2d82ca3ea8c12acb7b7558ec2f
DawidWy <71557844+DawidWy@users.noreply.github.com>
2025-02-21 12:23:28 +0100

    Add files via upload

---
+++ src/main/java/org/zerodebug/evemarket/EvemarketApplication.java
@@ -0,0 +1,13 @@
+package org.zerodebug.evemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvemarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvemarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerodebug/evemarket/EvemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerodebug.evemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvemarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
