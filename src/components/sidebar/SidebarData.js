8378910731629ceaa49955dee5699d781f8cab64
Mateusz Nowaczek <mateusznowaczekmojekonto@gmail.com>
2025-02-21 01:43:44 +0100

    Initial commit

---
+++ src/components/sidebar/SidebarData.js
@@ -0,0 +1,40 @@
+const treeData = [
+    {
+      name: "Ship SKINs",
+      children: [
+        {
+          name: "Battlecruisers",
+          children: [
+            {
+              name: "Advanced Battlecruisers",
+              children: [
+                {
+                  name: "Command Ships",
+                  children: [
+                    {
+                      name: "Amarr",
+                      children: [
+                        { name: "Absolution SKIN" },
+                        { name: "Damnation SKIN" }
+                      ]
+                    },
+                    {
+                      name: "Caldari",
+                      children: [
+                        { name: "Nighthawk Biosecurity Responders SKIN" },
+                        { name: "Nighthawk Blue Tiger SKIN" },
+                        { name: "Nighthawk Empyrean Outlaws SKIN" }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  
+  export default treeData;
+  
