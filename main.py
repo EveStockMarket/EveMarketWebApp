8031c0841a41b1528eaa168b1436dc8dce9cb4ad
Mateusz Nowaczek <mateusznowaczekmojekonto@gmail.com>
2025-03-10 23:44:46 +0100

    update

--- main.py
+++ main.py
@@ -6,29 +6,25 @@ from controllers.icon_controller import router as icon_router
 from controllers.market_controller import router as market_router 
 from controllers.market_controller import fetch_all_security_status
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 Starting security status updater...")
+    asyncio.create_task(fetch_all_security_status())  
+    yield 
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("🚀 Starting security status updater...")
-    asyncio.create_task(fetch_all_security_status())  # Uruchamiamy w tle
-    yield 
-
-app = FastAPI(lifespan=lifespan)
 app.include_router(icon_router)
 app.include_router(market_router)
 
 @app.get("/")
 async def root():
-    return {"message": "EVE Stock Market API is running!"}
-
-
-    
+    return {"message": "EVE Stock Market API is running with CORS!"}
